d3016cec5b9e6755a473e3afb1e2a96729271a74
Glandim <gabriel.landim@fatec.sp.gov.br>
2021-03-29 14:34:10 -0300

    Criação das rotas

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const AppStack = createStackNavigator();
+
+import home from './pages/home';
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+
+      <AppStack.Navigator screenOptions={{ headerShown: false }} lazy={true}>
+
+        <AppStack.Screen name='home' component={home} options={{unmountOnBlur:true}} />
+
+      </AppStack.Navigator>
+
+    </NavigationContainer>
+  );
+}
